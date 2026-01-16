e8666e7d099a8211d225ab871d444408dab7f1f4
Keshav <keshav.poha@gmail.com>
2026-01-16 14:36:46 +0530

    Template

--- my-addon/src/models/DocumentSandboxApi.ts
+++ my-addon/src/models/DocumentSandboxApi.ts
@@ -1,6 +1,4 @@
 // This interface declares all the APIs that the document sandbox runtime ( i.e. code.ts ) exposes to the UI/iframe runtime
-// Now simplified - we don't need sandbox APIs since createRenditions is available in UI runtime
-
 export interface DocumentSandboxApi {
-    ping(): Promise<string>;
+    createRectangle(): void;
 }
